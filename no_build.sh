ddc9448fa38ec983b5ee40a007980ebf828d262e
snayaksnayak <sinu.nayak2001@gmail.com>
2013-04-11 14:43:25 +0530

    added some scripts and images to run poweros

---
+++ no_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#make clean
+#make
+
+cp ./bin/kernel.bin ./bin/kernel
+
+losetup /dev/loop10 ./harddisk.img
+mount /dev/loop10 /mnt/floppy
+cp ./bin/kernel /mnt/floppy
+cp ./bin/kernel.bin /mnt/floppy
+umount /dev/loop10
+losetup -d /dev/loop10
+
+#../qemu/bin/qemu -L ../qemu/share/ -no-kqemu -m 64 -hda ./harddisk.img
+qemu -m 64 -vga vmware -serial stdio -hda ./harddisk.img
